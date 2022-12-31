e6f33ae7ee8b28aa42ee0c46782091561ee4b62e
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2022-12-31 21:31:11 +0800

    city router

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,14 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import('@/pages/home/Home')
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: () => import('@/pages/home/Home')
+  },
+  {
+    path: '/city',
+    name: 'City',
+    component: () => import('@/pages/city/City')
+  }]
 })
