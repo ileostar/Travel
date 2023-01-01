3b96d85e7bacb2a168d5e93a8120b6be73405a28
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2023-01-02 00:36:19 +0800

    perfomance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,8 +3,10 @@
     <city-header></city-header>
     <city-search></city-search>
     <city-list :cities="cities"
-               :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+               :hot="hotCities"
+               :letter="letter"></city-list>
+    <city-alphabet :cities="cities"
+                   @change="handleLetterChange"></city-alphabet>
   </div>
 </template>
 
@@ -25,7 +27,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -40,6 +43,9 @@ export default {
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
   },
   mounted () {
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -22,7 +22,8 @@
       </div>
       <div class="area"
            v-for="(item,key) of cities"
-           :key="key">
+           :key="key"
+           :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
           <div class="item border-bottom"
@@ -40,10 +41,19 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
