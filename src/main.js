b13665d5a1b4b4722c8150238b25b7fc9e6816f4
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2023-01-06 04:03:56 +0800

    fix:修复程序问题

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import fastClick from 'fastclick'
 import router from './router'
 import store from './store'
 import 'styles/reset.css'
@@ -16,7 +15,6 @@ import 'vue-awesome-swiper/node_modules/swiper/dist/css/swiper.css'
 Vue.use(VueAwesomeSwiper /* { default global options } */)
 
 Vue.config.productionTip = false
-fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
