d6ed03fa842bb1f992e4229304cf9f60c67bd070
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2023-01-09 08:31:14 +0800

    update

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+        <swiper-slide v-for="(item,key) in imgs" :key="key">
+          <img class="gallary-img" :src="item" />
+        </swiper-slide>
+        <div class="swiper-pagination"
+            slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      types: Array,
+      default () {
+        return ['http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg',
+        'http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png']
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow: inherit
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      height 0
+      width: 100%
+      padding-bottom: 100%
+      .gallary-img
+        width: 100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+</style>
